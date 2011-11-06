6931707c56ad722d057c3f630819668884e5b6fe
Paquitosoft <paquitosoftware@gmail.com>
2011-11-06 14:59:22 -0800

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -28,4 +28,9 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Help")
   end
   
+  it "should have a Sign up page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+  
 end
