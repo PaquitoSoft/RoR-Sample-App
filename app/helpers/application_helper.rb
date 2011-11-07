a545ff56fab0221cbc2362c726e3f284cd341f08
Paquitosoft <paquitosoftware@gmail.com>
2011-11-07 12:01:15 -0800

    Just before chapter 6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,11 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end 
   end
+
+  # Logo helper
+  def logo
+    return image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   
 end
