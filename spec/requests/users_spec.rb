6056d81fb7d77a1cfa4b1bc8dbb281a65505f7a6
Paquitosoft <paquitosoftware@gmail.com>
2011-11-22 11:07:51 -0800

    User signup complete

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,7 +7,29 @@ describe "Users" do
     describe "failure" do
       
       it "should not make a new user" do
-        
+        lambda do
+          visit signup_path
+          fill_in "Name", :with => ""
+          fill_in "Email", :with => ""
+          fill_in "Password", :with => ""
+          fill_in "Confirmation", :with => ""
+          click_button
+          response.should render_template('users/new')
+          response.should have_selector("div#error_explanation")
+        end.should_not change(User, :count)
+      end
+      
+      it "should make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Name", :with => "Example User"
+          fill_in "Email", :with => "user@emaple.com"
+          fill_in "Password", :with => "foobar"
+          fill_in "Confirmation", :with => "foobar"
+          click_button
+          response.should render_template('users/show')
+          response.should have_selector("div.flash.success", :content => "Welcome")
+        end.should change(User, :count).by(1)
       end
       
     end
